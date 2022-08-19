8f3f61304728198c41ce69186acba01fd238f1e5
DAWells <david.arthur.wells@gmail.com>
2022-08-19 16:39:59 +0100

    View supertype population coverage

--- code/demos/population_coverage.py
+++ code/demos/population_coverage.py
@@ -1,10 +1,16 @@
 """
 How much of the population is covered by the top n alleles?
+What is the population coverage of different supertypes?
 """
 
 import code.scrapeAF as scrapeAF
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Which supertypes are represented
+super1 = pd.read_csv("data/HLA1supertypes_Sidney2008.csv")
+super1.allele = super1.allele.apply(lambda x: x[:4]+':'+x[4:])
 
 AFtab = pd.read_csv("data/example/Uganda_raw.csv")
 AFtab = scrapeAF.formatAF(AFtab)
@@ -14,11 +20,17 @@ AFtab = scrapeAF.unmeasured_alleles(AFtab)
 
 # Weighted average alleles
 wav = scrapeAF.combineAF(AFtab)
-
+# Add HLA1 supertypes
+wav = pd.merge(wav, super1)
 wava = wav[wav.loci == "A"].sort_values('allele_freq', ascending=False, ignore_index = True)
 
-plt.bar(wava.allele, wava.allele_freq)
-plt.xticks(rotation=90)
+# Plot allele frequencies coloured by supertype
+sns.barplot(
+    y=wava.allele,
+    x=wava.allele_freq,
+    hue=wava.supertype,
+    dodge=False
+    )
 plt.show()
 
 plt.plot(wava.allele_freq.cumsum(), label="Cumulative AF")
@@ -26,3 +38,15 @@ plt.plot(wava.allele_freq.cumsum().apply(scrapeAF.population_coverage), label="C
 plt.axhline(y=1, color="black", linestyle="--")
 plt.legend()
 plt.show()
+
+for supertype in wava.supertype.unique():
+    print(supertype)
+    supertypedf = wava[wava.supertype == supertype].reset_index()
+    plt.plot(supertypedf.allele_freq.cumsum().apply(scrapeAF.population_coverage), label=supertype)
+plt.legend()
+plt.show()
+
+mask = wava.allele_freq.cumsum() > 0.95
+wava[~mask]
+wava[~mask].supertype.unique()
+
--- readme.md
+++ readme.md
@@ -18,7 +18,7 @@ import scrapeAF
 When selecting alleles to represent a population, all supertypes
 should be represented.
 
-When combining extimates of allele frequency larger samples should
+When combining estimates of allele frequency larger samples should
 weigh more heavily. However, when combining estiamtes that disagree
 the uncertainty on the estimate should be larger than when combining
 equivalent samples that agree.
@@ -26,3 +26,13 @@ equivalent samples that agree.
 When combining studies we must weight averages by sample size but
 when combining countries are we not better to weight by national population?
 Should sample size also have an effect?
+
+IEDB provide reference sets of HLA alleles designed to cover the
+majority of the "general population". However, all populations are
+not likely to be equally well covered by them. Using population
+specific allele frequencies we should be able to select more
+appropriate HLA sets. However, allele frequency is not the only
+determinent when selecting a pannel of alleles. The pannel must
+also contain representatives of commonly shared binding specificities
+(i.e., supertypes).
+
