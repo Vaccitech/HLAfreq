63d210fad3a9af581302c5ba30c43c45f3360b8e
DAWells <david.arthur.wells@gmail.com>
2022-08-22 12:45:11 +0100

    Prepare allele frequencies for dimension reduction

--- code/demos/globalPCA.py
+++ code/demos/globalPCA.py
@@ -35,6 +35,30 @@ for country in countries:
     wav['country'] = country
     wavs.append(wav)
 
-wavs = pd.concat(wavs, axis=0).reset_index()
+wavs = pd.concat(wavs, axis=0).reset_index(drop=True)
 
-scrapeAF.unmeasured_alleles(wavs, 'country')
+# Give record for all alleles to all countries
+wavs = scrapeAF.unmeasured_alleles(wavs, 'country')
+
+# Filter to single loci
+wavsa = wavs[wavs.loci == 'A']
+
+# Check all countries have the same number of alleles
+wavsa.groupby('country').allele.unique().apply(len)
+
+# Sort by allele then select a single country,
+# check that its alleles match a specified list
+# then use the frequencies as features
+wavsa = wavsa.sort_values('allele')
+
+sorted_alleles = sorted(list(wavsa.allele.unique()))
+
+AFeatures = []
+for country in countries:
+    mask = wavsa['country'] == country 
+    assert all(wavsa[mask].allele.unique() == sorted_alleles)
+    features = [country] + wavsa[mask].allele_freq.tolist()
+    AFeatures.append(features)
+
+# Dataframe of allele frequencies read for dimension reduction
+AFeatures = pd.DataFrame(AFeatures, columns = ['country'] + sorted_alleles)
