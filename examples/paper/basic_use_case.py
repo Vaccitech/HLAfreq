5b2a81a13ec5258eb7a5ebb77161f6033352d6cb
DAWells <david.arthur.wells@gmail.com>
2022-11-04 17:12:52 +0000

    Basic use case for paper

---
+++ examples/paper/basic_use_case.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from HLAfreq import makeURL, getAFdata, combineAF, plotAFprob, population_coverage
+
+base_url = makeURL("Mongolia", standard="g", locus="DQB1")
+aftab = getAFdata(base_url)
+aftab.population.unique()
+caf = combineAF(aftab)
+plotAFprob(caf, aftab, ncol=2, xmin=-0.02, xmax=0.5)
+
+caf = caf.sort_values('allele_freq', ascending=False, ignore_index=True)
+plt.scatter(caf.allele, caf.allele_freq.cumsum().apply(population_coverage))
+plt.grid(True); plt.xticks(rotation=90); plt.tight_layout()
+plt.xlabel('Allele'); plt.ylabel('Cumulative population coverage')
+plt.tight_layout(); plt.show()
