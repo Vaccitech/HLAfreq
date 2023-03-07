8b19902ec5dc7ff2cc596239f61a608f3d27bee7
DAWells <david.arthur.wells@gmail.com>
2023-03-07 10:39:41 +0000

    Add hdi plot to basic use case

--- examples/paper/basic_use_case.py
+++ examples/paper/basic_use_case.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 from HLAfreq import makeURL, getAFdata, combineAF, plotAF, population_coverage
+from HLAfreq import HLAfreq_pymc
 
 base_url = makeURL("Mongolia", standard="g", locus="DQB1")
 aftab = getAFdata(base_url)
-aftab.population.unique()
 caf = combineAF(aftab)
-plotAF(caf, aftab, credible_interval=0.95)
+hdi = HLAfreq_pymc.AFhdi(aftab, credible_interval=0.95)
+plotAF(caf, aftab, hdi=hdi, compound_mean=hdi)
 
 caf = caf.sort_values('allele_freq', ascending=False, ignore_index=True)
 plt.scatter(caf.allele, caf.allele_freq.cumsum().apply(population_coverage))
