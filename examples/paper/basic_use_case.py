b42df9b5486426ccf65fc7397dc909b6eed39cf0
DAWells <david.arthur.wells@gmail.com>
2023-07-05 11:09:22 +0100

    Simplify HLAfreq import in baseic use case for paper

--- examples/paper/basic_use_case.py
+++ examples/paper/basic_use_case.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
-from HLAfreq import makeURL, getAFdata, combineAF, plotAF, population_coverage
+import HLAfreq
 from HLAfreq import HLAfreq_pymc
 
-base_url = makeURL("Mongolia", standard="g", locus="DQB1")
-aftab = getAFdata(base_url)
-caf = combineAF(aftab)
+base_url = HLAfreq.makeURL("Mongolia", standard="g", locus="DQB1")
+aftab = HLAfreq.getAFdata(base_url)
+caf = HLAfreq.combineAF(aftab)
 hdi = HLAfreq_pymc.AFhdi(aftab, credible_interval=0.95)
-plotAF(caf, aftab, hdi=hdi, compound_mean=hdi)
+HLAfreq.plotAF(caf, aftab, hdi=hdi, compound_mean=hdi)
 
 caf = caf.sort_values('allele_freq', ascending=False, ignore_index=True)
-plt.scatter(caf.allele, caf.allele_freq.cumsum().apply(population_coverage))
+plt.scatter(caf.allele, caf.allele_freq.cumsum().apply(HLAfreq.population_coverage))
 plt.grid(True); plt.xticks(rotation=90); plt.tight_layout()
 plt.xlabel('Allele'); plt.ylabel('Cumulative population coverage')
 plt.tight_layout(); plt.show()
