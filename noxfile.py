2c629c5bba49dad6def07239be81de21f5b53c94
DAWells <david.arthur.wells@gmail.com>
2024-07-03 14:25:44 +0100

    update nox and github action testing to 3.10-3.12 as recommended https://scientific-python.org/specs/spec-0000/

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ def lint(session):
     session.run("flake8", "src", "--count", "--exit-zero", "--max-complexity=10", "--max-line-length=127", "--statistics")
 
 
-@nox.session(python=["3.8", "3.9", "3.10"])
+@nox.session(python=["3.10", "3.11", "3.12"])
 def tests(session):
     session.install("pytest")
     session.install(".")
