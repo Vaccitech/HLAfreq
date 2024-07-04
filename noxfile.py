3c266a3841ab4f7976195f649f4e2b8bb77056cd
DAWells <david.arthur.wells@gmail.com>
2024-07-04 10:29:44 +0100

    Add bandit to nox file

--- noxfile.py
+++ noxfile.py
@@ -8,6 +8,10 @@ def lint(session):
     session.run("flake8", "src", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics")
     session.run("flake8", "src", "--count", "--exit-zero", "--max-complexity=10", "--max-line-length=127", "--statistics")
 
+@nox.session
+def bandit(session):
+    session.install("bandit")
+    session.run("bandit", "-r", "src")
 
 @nox.session(python=["3.10", "3.11", "3.12"])
 def tests(session):
