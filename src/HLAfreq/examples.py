bd59f864801446f00336239cf1307cac41a061c0
DAWells <david.arthur.wells@gmail.com>
2024-08-07 17:26:07 +0100

    Correct the path to hosted html notebooks

--- src/HLAfreq/examples.py
+++ src/HLAfreq/examples.py
@@ -1,10 +1,10 @@
 """
 Several detailed `HLAfreq` examples are detailed in `examples/`.
 
-- [Single country](https://vaccitech.github.io/HLAfreq/single_country.ipynb) download and combine
-- [Multi-country](https://vaccitech.github.io/HLAfreq/multi_country.ipynb) download and combine, weight by population coverage
-- [Using priors](https://vaccitech.github.io/HLAfreq/working_with_priors.ipynb)
-- [Credible intervals](https://vaccitech.github.io/HLAfreq/credible_intervals.ipynb)
+- [Single country](https://vaccitech.github.io/HLAfreq/examples/single_country.ipynb) download and combine
+- [Multi-country](https://vaccitech.github.io/HLAfreq/examples/multi_country.ipynb) download and combine, weight by population coverage
+- [Using priors](https://vaccitech.github.io/HLAfreq/examples/working_with_priors.ipynb)
+- [Credible intervals](https://vaccitech.github.io/HLAfreq/examples/credible_intervals.ipynb)
 
 """
 
